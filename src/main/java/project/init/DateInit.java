43d08afebcedea818c4be938692552c8de3db42d
Vyagov <ivanviagov@abv.bg>
2020-08-02 23:57:25 +0300

    add templates, style, img

---
+++ src/main/java/project/init/DateInit.java
@@ -0,0 +1,12 @@
+package project.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DateInit implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
